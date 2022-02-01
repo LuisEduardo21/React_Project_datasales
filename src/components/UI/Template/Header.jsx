b96dba0bd205bc93dfccc1df14c3167b3d681059
luissilva <lfdasilva9@gmail.com>
2022-02-01 16:54:43 -0300

    ajuste finais

--- src/components/UI/Template/Header.jsx
+++ src/components/UI/Template/Header.jsx
@@ -1,10 +1,14 @@
 import './Header.css'
 import React from 'react'
+import Container from '@mui/material/Container';
+
 
 export default props =>
+   <Container>
     <header className='header d-none d-sm-flex flex-column'>
         <h1 className="mt-3">
             <i className={`fa fa-${props.icon}`}></i> {props.title}
         </h1>
         <p className="lead text-muted">{props.subtitle}</p>
     </header>
+    </Container>
